97eeafbcf128debca66d65705d34986b3b099177
Jatin Gupta <92937296+Jatin12a@users.noreply.github.com>
2025-05-02 02:03:30 +0530

    last change

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true, // enable default minification
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
